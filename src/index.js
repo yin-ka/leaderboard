cb91dd946858568f9fb4fcd7aa8fd7d9eb948a19
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-04-18 08:49:57 -0700

    Setup leadership structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const leaderboardData = [
+  { name: 'Name', score: 100 },
+  { name: 'Name', score: 30 },
+  { name: 'Name', score: 25 },
+  { name: 'Name', score: 87 },
+  { name: 'Name', score: 57 },
+  { name: 'Name', score: 10 },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const leaderBoardBody = document.querySelector('.table-body');
+const leaderboard = leaderboardData.map((data) => `
+  <tr>
+    <td scope="row">${data.name}:</td>
+    <td>${data.score}</td>
+  </tr>
+`).join('');
 
-  return element;
-}
-
-document.body.appendChild(component());
+leaderBoardBody.innerHTML = leaderboard;
